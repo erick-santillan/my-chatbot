e3ccf481d2bb90f614c19498fd6ed35983959edb
Erick Santillan <esn@novo.dk>
2025-07-04 12:13:54 +0200

    Remove vote functionality

--- lib/db/migrations/0005_wooden_whistler.sql
+++ lib/db/migrations/0005_wooden_whistler.sql
@@ -7,27 +7,5 @@ CREATE TABLE IF NOT EXISTS "Message_v2" (
 	"createdAt" timestamp NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "Vote_v2" (
-	"chatId" uuid NOT NULL,
-	"messageId" uuid NOT NULL,
-	"isUpvoted" boolean NOT NULL,
-	CONSTRAINT "Vote_v2_chatId_messageId_pk" PRIMARY KEY("chatId","messageId")
-);
 --> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "Message_v2" ADD CONSTRAINT "Message_v2_chatId_Chat_id_fk" FOREIGN KEY ("chatId") REFERENCES "public"."Chat"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
 --> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "Vote_v2" ADD CONSTRAINT "Vote_v2_chatId_Chat_id_fk" FOREIGN KEY ("chatId") REFERENCES "public"."Chat"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "Vote_v2" ADD CONSTRAINT "Vote_v2_messageId_Message_v2_id_fk" FOREIGN KEY ("messageId") REFERENCES "public"."Message_v2"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
